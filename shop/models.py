3d2ba7e62db1317d16f9296bae9efb546889acef
Almaz Galimov <almazwork@gmail.com>
2025-02-14 16:12:53 +0500

    Add orders templates

--- shop/models.py
+++ shop/models.py
@@ -3,11 +3,11 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ("name",)
+        ordering = ["name"]
         indexes = [
             models.Index(fields=["name"]),
         ]
@@ -25,8 +25,8 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category, related_name="products", on_delete=models.CASCADE
     )
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -35,7 +35,7 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ("name",)
+        ordering = ["name"]
         indexes = [
             models.Index(fields=["id", "slug"]),
             models.Index(fields=["name"]),
--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import product_list, product_detail
+from . import views
 
 app_name = "shop"
 
 urlpatterns = [
-    path("", product_list, name="product_list"),
-    path("<slug:category_slug>", product_list, name="product_list_by_category"),
-    path("<int:id>/<slug:slug>", product_detail, name="product_detail"),
+    path("", views.product_list, name="product_list"),
+    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
+    path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
 ]
