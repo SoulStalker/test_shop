efe4d31db5d6100dd4e1498f3786b78afb40b829
Almaz Galimov <almazwork@gmail.com>
2025-02-19 18:16:53 +0500

    Add templates

--- payment/views.py
+++ payment/views.py
@@ -14,8 +14,8 @@ def payment_process(request):
     order_id = request.session.get("order_id", None)
     order = get_object_or_404(Order, id=order_id)
     if request.method == "POST":
-        success_url = request.build_absolute_url(reverse("payment:completed"))
-        cancel_url = request.build_absolute_url(reverse("payment:canceled"))
+        success_url = request.build_absolute_uri(reverse("payment:completed"))
+        cancel_url = request.build_absolute_uri(reverse("payment:canceled"))
         session_data = {
             "mode": "payment",
             "client_reference_id": order.id,
@@ -24,16 +24,18 @@ def payment_process(request):
             "line_items": [],
         }
         for item in order.items.all():
-            session_data["line_items"].append({
-                "price_data": {
-                    "unit_amount": int(item.price * Decimal("100")),
-                    "currency": "usd",
-                    "product_data": {
-                        "name": item.product.name,
+            session_data["line_items"].append(
+                {
+                    "price_data": {
+                        "unit_amount": int(item.price * Decimal("100")),
+                        "currency": "usd",
+                        "product_data": {
+                            "name": item.product.name,
+                        },
                     },
-                },
-                "quantity": item.quantity,
-            })
+                    "quantity": item.quantity,
+                }
+            )
         session = stripe.checkout.Session.create(**session_data)
         return redirect(session.url, code=303)
     else:
